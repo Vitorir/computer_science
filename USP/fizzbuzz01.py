8065e0f375505b0f190b7d8e1f4c5e3657470574
Vitorir <vitorireboucas@edu.unifor.br>
2023-02-07 23:57:53 -0300

    Add condicionals and loop experiments

---
+++ USP/fizzbuzz01.py
@@ -0,0 +1,19 @@
+n = int(input("Digite um numero: "))
+if (n % 5 == 0 and n % 3 == 0):
+    print("FizzBuzz")
+elif (n % 3 == 0):
+    print("Fizz")
+elif (n % 5 == 0):
+    print("Buzz")
+else:
+    print(n)
+
+n = int(input("Digite um numero: "))
+if (n % 3 == 0):
+    print("Frizz")
+elif (n % 3 !=0):
+    print(n)
+if (n % 5 == 0):
+    print("Buzz")
+elif (n % 5 != 0):
+    print(n)
